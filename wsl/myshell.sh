98161a8d5e318c433202ef84c4ccf70427ffc063
yoojj <59761257+yoojj@users.noreply.github.com>
2023-06-02 12:50:55 +0900

    Update myshell.sh

--- wsl/myshell.sh
+++ wsl/myshell.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
+nvm_version="v0.39.3"
+node_version="10"
+
+
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+
 sudo apt update -y 
 sudo apt upgrade -y
  
-sudo apt install -y build-essential ca-certificates libssl-dev 
-sudo apt install -y net-tools zip unzip wget curl vim
+sudo apt install -y build-essential libssl-dev  
+sudo apt install -y ca-certificates
+sudo apt install -y net-tools zip unzip wget curl vim cmake podman  
+
 
 sudo apt install -y openssh-server
 sudo ufw allow ssh
+
+
+sudo wget -q https://apt.thoughtbot.com/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg
+echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+sudo apt update
+sudo apt install rcm
+
 
 sudo apt install -y git 
 git config --global user.name "yjj"
 git config --global user.username "yjj"
 git config --global user.email "yoojjxyz@gmail.com"
-git config --global init.defaultBranch 'main'
+git config --global init.defaultBranch "main"
 git config --global credential.helper store
 
-
-bash_profile="$HOME/.bash_profile"
-bashrc="$HOME/.bashrc"
-
-if [[ ! -f "$bash_profile" ]]; then
-  touch "$bash_profile"
-fi
-
-if [[ ! -f "$bashrc" ]]; then
-  touch "$bashrc"
-fi
-
-
-echo ">>> install nvm"
-nvm_version="v0.39.3"
-node_version="10"
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_version}/install.sh | bash 
-source "$bashrc"
-
-nvm install "$node_version"
-nvm use "$node_version"
-nvm alias default "$node_version"
